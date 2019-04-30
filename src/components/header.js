1e276dca0ff9135daa95efa0b21b2ed27ac973f2
maharoz <mahruj66@gmail.com>
2019-04-30 22:49:08 +0600

    Styling the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,33 @@
 //import libraries for making component
 import React from 'react';
-import {Text} from 'react-native';
+import {Text,View} from 'react-native';
 
 
 //make a component
 const Header =()=>{
-    const { textStyle } = styles;
-    return <Text style={textStyle}>Albums</Text> 
+    const { textStyle,viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+        <Text style={textStyle}>Albums</Text>
+        </View>
+    );
 };
 
 const styles ={
+    viewStyle:{
+        backgroundColor:'#F8F8F8',
+        justifyContent :'center',
+        alignItems : 'center',
+        height : 60,
+        paddingTop : 15,
+        shadowColor : '#000',
+        shadowOffset:{width : 0 , height:2},
+        shadowOpacity : 0.9,
+        elevation:2,
+        position :'relative'
+    },
    textStyle : {
-    fontSize :200
+    fontSize :20
    }
 };
 
