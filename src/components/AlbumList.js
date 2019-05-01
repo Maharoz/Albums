52e18b31c85e5977e2f78b2c24b5c37756abcc12
maharoz <mahruj66@gmail.com>
2019-05-01 20:39:24 +0600

    Installing axios and create http requests

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,12 @@
 import React,{ Component } from 'react';
 import {View,Text} from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component{
     componentWillMount(){
-        console.log('Component will mount in album list');
+        //console.log('Component will mount in album list');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response));
     }
     render(){
         return(
